a8241c832d90ef290d4e8d1e5c5b3368e7494ff4
rsgc-leder-b <bleder@rsgc.on.ca>
2017-05-11 08:51:27 -0400

    Added rendering support for stochastic L systems

--- Animation/LindenmayerSystem.swift
+++ Animation/LindenmayerSystem.swift
@@ -7,7 +7,7 @@ public class LindenmayerSystem {
 	var n : Int                         // number of times the production rule is applied
 	var word : [String] = []            // the word that will be rendered
 	// is rendered with an animation, step by step
-	var rules : [String : String] = [:]
+	var rules : [Character : [String]] = [:]
 	var stochastic = false
 	
 	public struct Colour {
@@ -25,7 +25,7 @@ public class LindenmayerSystem {
 	public init(angle : Degrees,
 	            axiom : String,
 	            generations : Int,
-	            rules : [String : String]) {
+	            rules : [Character : [String]]) {
 		
 		// Initialize stored properties
 		self.angle = angle
@@ -33,11 +33,6 @@ public class LindenmayerSystem {
 		self.n = generations
 		self.rules = rules
 		self.word.append(axiom)   // The first word is the axiom
-		for rule in rules {
-			if rule.value[rule.value.index(rule.value.startIndex, offsetBy: 1)] == "/" {
-				stochastic = true
-			}
-		}
 		// Apply the production rule
 		applyRules()
 		
@@ -55,37 +50,25 @@ public class LindenmayerSystem {
 //		// Apply the production rule
 		applyRules()
 	}
-	
 	func applyRules() {
-		
 		// See if word needs to be re-written
 		if n > 0 {
-			
 			// Apply the production rule "n" times
 			for i in 1...n {
-				
 				// Create a new word
 				var newWord = ""
-				
 				// Inspect each character of existing word
 				for character in word[i - 1].characters {
-					if let charRule = rules[String(character)] {
-						newWord.append(charRule)
+					if let charRule = rules[character] {
+						newWord.append(charRule[random(from: 0, toButNotIncluding: charRule.count)])
 					} else {
 						// just copy what's in the existing word to the new word
 						newWord.append(character)
 					}
-					
 				}
-				
 				// Add the re-written word to the system
 				word.append(newWord)
-				
 			}
-			
 		}
-		print("Words:")
-		print(word)
 	}
-	
 }
--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -16,19 +16,23 @@ class Sketch : NSObject {
     
     // Create the basic L-systems
     let kochSnowflake : LindenmayerSystem
-//    let kochIsland : LindenmayerSystem
-//    let kochSwirl : LindenmayerSystem
-//    let kochConstruction : LindenmayerSystem
-    
+
     // Create the visualizations of the snowflake
     let smallKochSnowflake : VisualizedLindenmayerSystem
     let mediumKochSnowflake : VisualizedLindenmayerSystem
+
+    var states : [systemState] = []
     
-    // Create visualizations of other systems
-//    let largeKochIsland : VisualizedLindenmayerSystem
-//    let mediumKochSwirl : VisualizedLindenmayerSystem
-//    let mediumConstruction : VisualizedLindenmayerSystem
-    
+    struct systemState {
+        var x : Int
+        var y : Int
+        var angle : Degrees
+        init(x : Int, y : Int, angle : Degrees) {
+            self.y = y
+            self.x = x
+            self.angle = angle
+        }
+    }
     // This runs once, equivalent to setup() in Processing
     override init() {
         
@@ -39,8 +43,8 @@ class Sketch : NSObject {
         kochSnowflake = LindenmayerSystem(angle: 60,
                                           axiom: "F++F++F",
                                           generations: 3,
-                                          rules: ["F" : "1F-2F++3F-4F"])
-        
+                                          rules: ["F" : ["1F-2F++3F-4F"]])
+
         // Visualize this as a small snowflake
         smallKochSnowflake = VisualizedLindenmayerSystem(with: kochSnowflake,
                                                          length: 90,
@@ -64,59 +68,6 @@ class Sketch : NSObject {
                                                                    "2" : LindenmayerSystem.Colour(h: 110, s: 100, b: 100),
                                                                    "3" : LindenmayerSystem.Colour(h: 170, s: 100, b: 100),
                                                                    "4" : LindenmayerSystem.Colour(h: 230, s: 100, b: 100)])
-        
-        /*
-        // Set up a Koch Island
-        kochIsland = LindenmayerSystem(angle: 90,
-                                       axiom: "F-F-F-F",
-                                       rule: "F-F+F+FF-F-F+F",
-                                       generations: 5,
-                                       colors: ["1" : LindenmayerSystem.Colour(h: 100, s: 100, b: 100),
-                                                "2" : LindenmayerSystem.Colour(h: 50, s: 100, b: 100)],
-                                       rules: ["F" : "F-F+F+FF-F-F+F"])
-        
-        // Visualize the Koch Island
-        largeKochIsland = VisualizedLindenmayerSystem(with: kochIsland,
-                                                       length: 300,
-                                                       reduction: 4,
-                                                       x: 100,
-                                                       y: 400,
-                                                       direction: 10)
-        
-        // Set up a Koch Swirl
-        kochSwirl = LindenmayerSystem(angle: 90,
-                                      axiom: "-F",
-                                      rule: "F+F-F-F+F",
-                                      generations: 4,
-                                      colors: ["1" : LindenmayerSystem.Colour(h: 100, s: 100, b: 100),
-                                               "2" : LindenmayerSystem.Colour(h: 50, s: 100, b: 100)],
-                                      rules: ["F" : "F+F-F-F+F"])
-        
-        // Visualize the Koch Swirl
-        mediumKochSwirl = VisualizedLindenmayerSystem(with: kochSwirl,
-                                                      length: 300,
-                                                      reduction: 3,
-                                                      x: 250,
-                                                      y: 400,
-                                                      direction: 0)
-
-        // Set up another Koch construction
-        kochConstruction = LindenmayerSystem(angle: 90,
-                                             axiom: "F-F-F-F",
-                                             rule: "FF-F-F-F-F-F+F",
-                                             generations: 3,
-                                             colors: ["1" : LindenmayerSystem.Colour(h: 100, s: 100, b: 100),
-                                                      "2" : LindenmayerSystem.Colour(h: 50, s: 100, b: 100)],
-                                             rules: ["F" : "FF-F-F-F-F-F+F"])
-        
-        // Visualize this other Koch construction
-        mediumConstruction = VisualizedLindenmayerSystem(with: kochConstruction,
-                                                         length: 200,
-                                                         reduction: 4,
-                                                         x: 250,
-                                                         y: 350,
-                                                         direction: 0)
-        */
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
         
@@ -125,8 +76,12 @@ class Sketch : NSObject {
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
         
+        canvas.saveState()
+        
         // Render the current system
         canvas.renderAnimated(system: mediumKochSnowflake, generation: 2)
+        
+        canvas.restoreState()
         
     }
     
